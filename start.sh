985d1e1b29b2f1d61ca6922d2af618efc344c453
Oleg Fedoseev <o.fedoseev@office.ngs.ru>
2015-11-27 14:49:35 +0600

    Small fixes

--- start.sh
+++ start.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-mkdir /root/.ssh && ssh-keygen -t rsa -b 2048 -P "satis" -f /root/.ssh/id_rsa
+mkdir /root/.ssh && ssh-keygen -t rsa -b 2048 -P "" -f /root/.ssh/id_rsa
 
 echo -e "\n\nPublic key for Satis:"
 cat /root/.ssh/id_rsa.pub
 echo -e "\n\n"
+
+echo "Creating known_hosts..."
+for domain in $PRIVATE_DOMAINS ; do
+    ssh-keyscan -t rsa $domain >> /root/.ssh/known_hosts
+done
+
+chown -R www-data:www-data /var/www/satisfy/web/dist /var/www/composer
+cp -r /root/.ssh /var/www/.ssh && chown -R www-data:www-data /var/www/.ssh
 
 exec apache2-foreground
